ca81df6125cb5623a32a426259936032d6dc47b9
songhaechan <bukak2019@naver.com>
2024-11-05 16:39:13 +0900

    Bipartite improve

--- src/leetcode/Bipartite.java
+++ src/leetcode/Bipartite.java
@@ -10,28 +10,20 @@ public class Bipartite {
 
         Deque<Integer> que = new ArrayDeque();
         for (int i = 0; i < graph.length; i++) {
+            if (color[i]==1 || color[i]==2) {
+                continue;
+            }
             color[i] = 1;
             que.add(i);
             while (!que.isEmpty()) {
                 int cur = que.poll();
-                int newColor = 0;
-                if (color[cur]==1) {
-                    newColor = 2;
-                } else if (color[cur]==2) {
-                    newColor = 1;
-                }
                 for (int next : graph[cur]) {
                     if (color[cur]==color[next]) {
-                        System.out.println(cur);
-                        System.out.println(color[cur]);
-                        System.out.println(next);
-                        System.out.println(color[next]);
-                        System.out.println(newColor);
                         return false;
                     }
                     if (color[next]==0) {
                         que.add(next);
-                        color[next] = newColor;
+                        color[next] = 3 - color[cur];
                     }
                 }
             }
